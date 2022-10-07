24ba0abbbdad9493215e76f829167902dd5865e2
OttoCarius <ottocarius1993@gmail.com>
2022-10-07 16:34:39 +0300

    global-style + size + fonts

---
+++ src/components/Background/Background.styled.jsx
@@ -0,0 +1,8 @@
+// import { breakpoints } from '../../Utils/size';
+// import styled from 'styled-components';
+// import straw from '../../img/strawberry-desktop.png';
+// import ban from '../../img/banana-tablet.png';
+// import leaves from '../../img/leaves-tablet.png';
+// import vector from '../../img/vector-tablet.png';
+// import frame from '../../img/desktop.png';
+// import vector_d from '../../img/vector-desktop.png';
