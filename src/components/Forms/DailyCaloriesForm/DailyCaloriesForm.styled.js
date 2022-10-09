5b317d55cd1ecd6f19b5c575a6660204a9076a21
Yana-Palam <tana.palam@gmail.com>
2022-10-10 00:27:22 +0300

    add adaptive main page

--- src/components/Forms/DailyCaloriesForm/DailyCaloriesForm.styled.js
+++ src/components/Forms/DailyCaloriesForm/DailyCaloriesForm.styled.js
@@ -1,32 +1,43 @@
 import styled from 'styled-components';
-// import { Button } from 'components/Buttons/BattonMainActive/BattonMainActive.styled';
-
-// export const StyledButton = styled.btn`
-//   ${Button};
-//   margin-left: 339px;
-// `;
+import { breakpoints } from 'Utils/size';
 
 export const Form = styled.form`
   font-family: Verdana;
   line-height: 1.21;
   letter-spacing: 0.04em;
+  @media ${breakpoints.maxMobile} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 40px;
+  gap: 32px;
+  @media ${breakpoints.maxMobile} {
+    margin-bottom: 32px;
+  }
+  @media ${breakpoints.minTablet} {
+    gap: 40px;
+    margin-bottom: 40px;
+  }
 `;
 export const FormWrapper = styled.div`
-  display: flex;
-  gap: 32px;
-  margin-bottom: 60px;
+  margin-bottom: 40px;
+
+  @media ${breakpoints.minTablet} {
+    display: flex;
+    gap: 32px;
+    margin-bottom: 60px;
+  }
 `;
 
 export const Label = styled.label`
   font-weight: 700;
   font-size: 14px;
-  color: #9b9faa;
+  color: var(--secondary-text-color);
 `;
 
 export const Input = styled.input`
@@ -35,42 +46,42 @@ export const Input = styled.input`
   height: 20px;
   outline: none;
   font-family: inherit;
-  font-size: 1rem;
+  font-size: 14px;
   line-height: 1.5;
   color: #212529;
-  background-color: #fff;
+  background-color: transparent;
   background-clip: padding-box;
-  border: none;
-  border-bottom: 1px solid #bdbdbd;
+  border-bottom: 1px solid var(--border-color);
   transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
 `;
 
-// export const Button = styled.button`
-//   background: #fc842d;
-//   border: 2px solid #fc842d;
-//   border-radius: 30px;
-//   width: 182px;
-//   height: 44px;
-//   font-family: Verdana;
-//   font-weight: 700;
-//   font-size: 14px;
-//   line-height: 1.21;
-//   display: flex;
-//   align-items: center;
-//   text-align: center;
-//   letter-spacing: 0.04em;
-//   justify-content: center;
-//   color: #ffffff;
-//   margin-left: 339px;
-// `;
+export const Button = styled.button`
+  background: var(--orange-bgc);
+  border: 2px solid var(--orange-color);
+  border-radius: 30px;
+  padding: 12px 49px;
+  font-family: Verdana;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.21;
+  letter-spacing: 0.04em;
+  color: var(--white);
+  box-shadow: 0px 4px 10px rgba(252, 132, 45, 0.5);
+  cursor: pointer;
+  /* margin-bottom: 77px; */
+
+  @media ${breakpoints.desktop} {
+    margin-left: 339px;
+  }
+`;
 
 export const RadioTitle = styled.h2`
   font-weight: 700;
   font-size: 14px;
-  color: #9b9faa;
+  color: var(--secondary-text-color);
   &::after {
     margin-top: 20px;
     margin-bottom: 20px;
@@ -78,7 +89,7 @@ export const RadioTitle = styled.h2`
     display: block;
     height: 1px;
     width: 240px;
-    background-color: #bdbdbd;
+    background-color: var(--border-color);
   }
 `;
 
@@ -89,21 +100,21 @@ export const RadioWrapper = styled.div`
 `;
 
 export const RadioLabel = styled.label`
-  color: #9b9faa;
+  color: var(--secondary-text-color);
   display: flex;
   flex-direction: row-reverse;
   gap: 4px;
 
   &:checked {
-    color: #fc842d;
+    color: var(--orange-color);
   }
 `;
 
 export const RadioInput = styled.input`
-  color: #9b9faa;
+  color: var(--secondary-text-color);
 
   &:checked {
-    border: 6px solid #fc842d;
-    color: #fc842d;
+    border: 6px solid var(--orange-color);
+    color: var(--orange-color);
   }
 `;
