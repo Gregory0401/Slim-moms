edfe6e72d65c6a1e85999fcbf771fa6306ce5ead
Yana-Palam <tana.palam@gmail.com>
2022-10-13 14:15:20 +0300

    fix header

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,7 @@
 import { Navigation } from '../Navigation/Navigation';
 import { UserAuthMenu } from '../UserAuthMenu/UserAuthMenu';
 import { AuthNavigation } from '../AuthNavigation/AuthNavigation';
+import { useMedia } from 'react-use';
 
 import { useSelector } from 'react-redux';
 import {
@@ -8,14 +9,14 @@ import {
   selectAccessToken,
 } from '../../Redux/Auth/authSelectors';
 import { Header, Wrapper, Menu } from './AppBar.styled';
-import {ButtonBurger} from '../Buttons/ButtonBurger/ButtonBurger';
+import { ButtonBurger } from '../Buttons/ButtonBurger/ButtonBurger';
 import { useState } from 'react';
 import BurgerMenu from 'components/Modal/BurgerMenu/BurgerMenu';
-import {Burger} from '../UserAuthMenu/UserAuthMenu.styled'
-
+import { Burger } from '../UserAuthMenu/UserAuthMenu.styled';
 
 export const AppBar = () => {
   const [showModal, setShowModal] = useState(false);
+  const isWide = useMedia('(max-width: 767px)');
 
   const onToggleModal = () => {
     setShowModal(prevState => !prevState);
@@ -25,20 +26,39 @@ export const AppBar = () => {
   const token = useSelector(selectAccessToken);
   return (
     <Header>
+      {isWide ? (
+        <>
+          <Wrapper>
+            <Navigation />
+            {!isLoggedIn && !token && <AuthNavigation />}
+            {isLoggedIn && (
+              <Burger>
+                <ButtonBurger onClick={onToggleModal} />
+              </Burger>
+            )}
+          </Wrapper>
+          {isLoggedIn && token && <UserAuthMenu />}
+        </>
+      ) : (
+        <>
+          <Wrapper>
+            <Navigation />
+            {!isLoggedIn && !token && <AuthNavigation />}
+            {isLoggedIn && token && <UserAuthMenu />}
+            {isLoggedIn && (
+              <Burger>
+                <ButtonBurger onClick={onToggleModal} />
+              </Burger>
+            )}
+          </Wrapper>
+        </>
+      )}
 
-      <Wrapper>
-        <Navigation />
-        {!isLoggedIn && !token && <AuthNavigation /> }
-        {isLoggedIn &&  <Burger><ButtonBurger onClick={onToggleModal}/></Burger>}
-      </Wrapper>
-      {isLoggedIn && token && <UserAuthMenu />}
-      
-      {showModal && <Menu><BurgerMenu /></Menu> }
+      {showModal && (
+        <Menu>
+          <BurgerMenu />
+        </Menu>
+      )}
     </Header>
-    
-    
-    
-    
-     
   );
 };
